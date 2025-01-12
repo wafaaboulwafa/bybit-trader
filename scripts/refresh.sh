b538a2b5f95c3e8a768d236486a914deee53192e
Wafa <wafaaboulwafa@hotmail.com>
2025-01-12 15:18:51 +0300

    install

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -3,8 +3,13 @@
 BRANCH="main"
 SERVICE_NAME="bybit-trader"
 
+# stop the service
 sudo systemctl stop "$SERVICE_NAME" || { echo "Failed to install service $SERVICE_NAME. Exiting."; exit 1; }
 sudo systemctl disable "$SERVICE_NAME" || { echo "Failed to start service $SERVICE_NAME. Exiting."; exit 1; }
+
+# Reset any local changes
+echo "Resetting local changes in the repository"
+git reset --hard || { echo "Failed to reset changes. Exiting."; exit 1; }
 
 # Fetch and pull the latest changes
 echo "Fetching and pulling the latest changes from branch $BRANCH"
