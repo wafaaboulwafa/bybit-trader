7ee79147828478abeb66193af4947796aec39c90
Wafa Aboulwafa <67149147+wa250031@users.noreply.github.com>
2025-01-12 23:01:50 +0300

    delete

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -1,14 +1,10 @@
-import emaCrossStrategy from "./emaCross";
-import rsiEmaCrossStrategy from "./rsiEmaCross";
-import bbCrossStrategy from "./bbCross";
 import { OnStrategyType } from "../service/types";
-import wychoffStrategy from "./wychoff";
 import defaultStrategy from "./default";
+import bbCrossStrategy from "./bbCross";
+import wychoffStrategy from "./wychoff";
 
 export default new Map<string, OnStrategyType>([
   ["default", defaultStrategy],
   ["bbCross", bbCrossStrategy],
   ["wychoff", wychoffStrategy],
-  // ["emaCross", emaCrossStrategy],
-  // ["rsiEmaCross", rsiEmaCrossStrategy],
 ]);
