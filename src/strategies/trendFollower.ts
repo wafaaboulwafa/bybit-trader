a7f6b249522bf730d5f2305e4e79df8af747790b
wafaaboulwafa <wafa.x@outlook.com>
2025-01-29 05:35:34 +0300

    Update trendFollower.ts

--- src/strategies/trendFollower.ts
+++ src/strategies/trendFollower.ts
@@ -20,7 +20,7 @@ import {
 } from "../repository/tradeProcessing";
 
 const stopLossRatio: number = 2;
-const takeProfitRatio: number = stopLossRatio * 3;
+const takeProfitRatio: number = stopLossRatio * 2;
 
 interface TimeFrameAnalysesType {
   highTrend: "Uptrend" | "Downtrend" | "Sideways" | undefined;
