98a832397af401c7fb46080e1dfb96d3858cec16
Wafa <wafaaboulwafa@hotmail.com>
2025-01-31 15:37:56 +0300

    rename

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -2,11 +2,11 @@ import { OnStrategyType } from "../service/types";
 import defaultStrategy from "./default";
 import bbCrossStrategy from "./bbCross";
 import wychoffStrategy from "./wychoff";
-import trendFollowerStrategy from "./trendFollower";
+import trendFollowPullbackStrategy from "./trendFollowPullback";
 
 export default new Map<string, OnStrategyType>([
   ["default", defaultStrategy],
   ["bbCross", bbCrossStrategy],
   ["wychoff", wychoffStrategy],
-  ["trendFollower", trendFollowerStrategy],
+  ["trendFollowPullback", trendFollowPullbackStrategy],
 ]);
