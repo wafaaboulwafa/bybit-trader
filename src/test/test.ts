2059074fb8c0b0de0da226f7102eb6d4652c40d3
Wafa <wafaaboulwafa@hotmail.com>
2025-02-01 19:43:35 +0300

    Strategy test

--- src/test/test.ts
+++ src/test/test.ts
@@ -8,8 +8,10 @@ const pairName = "BTCUSDT";
 const baseCoin = "BTC";
 const quoteCoin = "USDT";
 
+const testUTCDate = "2025-01-20T09:20:00Z";
+
 setTimeout(async () => {
-  const endDate = new Date();
+  const endDate = new Date(Date.parse(testUTCDate));
 
   const pair = new PairRepo(
     pairName,
@@ -27,6 +29,20 @@ setTimeout(async () => {
     const timeFrameRepo = pair.getTimeFrame(timeFrmae);
     if (!timeFrameRepo) continue;
     await timeFrameRepo.init(pair.pair, timeFrmae, pair.isFuture, endDate);
+  }
+
+  const sortedFrames = [...timeFrames];
+  sortedFrames.sort((a, b) => parseInt(a) - parseInt(b));
+
+  let currentPrice: number = 0;
+  for (const timeFrmae of sortedFrames) {
+    const timeFrameRepo = pair.getTimeFrame(timeFrmae);
+    if (!timeFrameRepo) continue;
+    if (currentPrice === 0) currentPrice = timeFrameRepo?.closePrice[0];
+    else {
+      timeFrameRepo.closePrice[0] = currentPrice;
+      timeFrameRepo.candle[0].closePrice = currentPrice;
+    }
   }
 
   for (const timeFrmae of timeFrames) {
