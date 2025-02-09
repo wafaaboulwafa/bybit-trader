2f688f1eb169ccdd301106a199ce0341a89d6a05
Wafa <wafaaboulwafa@hotmail.com>
2025-02-09 19:35:27 +0300

    Fix file name

--- src/strategies/index.ts
+++ src/strategies/index.ts
@@ -2,7 +2,7 @@ import { OnStrategyType } from "../service/types";
 import defaultStrategy from "./default";
 import wychoffStrategy from "./wychoff";
 import trendFollowPullbackStrategy from "./trendFollowPullback";
-import fiveMinInvertedStrategy from "./fiveminInverted";
+import fiveMinInvertedStrategy from "./fiveMinInverted";
 
 export default new Map<string, OnStrategyType>([
   ["default", defaultStrategy],
